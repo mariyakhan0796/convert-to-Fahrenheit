7d698b68bdf98326bf4d79c490c8d1e5cbb598b8
mariyakhan0796 <mariyakhan0796@gmail.com>
2024-08-13 17:31:45 +0500

    Temprature converter

---
+++ index.js
@@ -0,0 +1,19 @@
+var userInput = prompt('Enter Temperatur in Celsius');
+var celsius = parseFloat(userInput);
+
+var fahrenheit = 0;
+fahrenheit = (celsius * 9/5) + 32;
+console.log('Temperature in Fahrenheit is = ',fahrenheit );
+
+
+
+// Task:
+// Get Temperature in fahrenheit,
+// convert it into celsius, kelvin
+/*
+formula:
+celsius = 5/9 * (fahrenheit - 32);
+kelvin = Celsius + 273;
+
+*/
+
